604f0c251a6aeba8e0502031e9b6593b6c728207
unknown <s.t.angel@abv.bg>
2021-11-10 13:00:26 +0200

    modified:   exam_1/1_b.cpp 	new file:   homework_1/homework_1.pdf

--- exam_1/1_b.cpp
+++ exam_1/1_b.cpp
@@ -8,7 +8,7 @@ int main()
 	cout << "y= ";
 	cin >> y;
 
-
+    // Wrong
 	bool inFigure = ((x >= -2 && x <= 2) && (y >= -2 && y <= 2) && (x * x + y * y >= 4)) || (x >= 0 && x <= 2) && (y >= 0 && y <= 2);
 
 	return 0;
